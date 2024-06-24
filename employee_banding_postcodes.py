11b72b6a111432771c422a5e17d877bc6021923a
Emily-OBrien-NHS <e.obrien6@nhs.net>
2024-06-24 13:51:06 +0100

    Add pension data to lookup table

--- employee_banding_postcodes.py
+++ employee_banding_postcodes.py
@@ -62,6 +62,11 @@ for group in LL_df['Band Groups'].drop_duplicates().tolist():
 # =============================================================================
 # #Table Data
 # =============================================================================
+#read in pension data
+pension = pd.read_excel('C:/Users/obriene/Projects/Mapping/Pension Opt Out Month 2 2024.xlsx')
+pension = pension.rename(columns={'Band ':'Banding', 'Age Band':'AgeBand'})
+
+#Functions
 def parse_date(td):
     #Conerts difference between two dates into string of xY xm
     resYear = ((td.dt.days)/364.0).astype(float)                 # get the number of years including the the numbers after the dot
@@ -69,12 +74,14 @@ def parse_date(td):
     resYear = (resYear).astype(int).astype(str)
     return resYear + "Y " + resMonth + "m"
 
-def group_by_band(df, cols):
+def group_by_band(df, pen, cols):
      #Function to group up data by different values and produce the results.
      df = df.groupby(cols).agg({'Band No':'count', 'FTE':'sum', 'Days in Position':['mean', 'max']}).reset_index()
      df.columns = cols + ['Headcount', 'FTE', 'Average Time in Position', 'Max Time in Position']
      df['Average Time in Position'] = parse_date(df['Average Time in Position'])
      df['Max Time in Position'] = parse_date(df['Max Time in Position'])
+     #Add pension data
+     df = df.merge(pen.groupby(cols, as_index=False)['Pension Opt Out'].count(), on=cols, how='left')
      return df
 
 #Select/add required columns
@@ -83,8 +90,8 @@ df['Days in Position'] = (pd.Timestamp.now() - pd.to_datetime(df['StartDateInPos
 df['FTE'] = df['FTE'].astype(float)
 
 #Get the grouped tables required
-band_name = group_by_band(df, ['Banding'])
-age_band = group_by_band(df, ['Banding', 'AgeBand'])
+band_name = group_by_band(df, pension, ['Banding'])
+age_band = group_by_band(df, pension, ['Banding', 'AgeBand'])
 lookup_col = age_band[['Banding', 'AgeBand']].astype(str).agg(' '.join, axis=1)
 age_band.insert(loc=0, column='lookup', value=lookup_col)
 
@@ -92,7 +99,7 @@ age_band.insert(loc=0, column='lookup', value=lookup_col)
 ##############To excel ##################
 #Lists for formatting
 ages = ['<=20 Years', '21-25', '26-30', '31-35', '36-40', '41-45', '46-50', '51-55', '56-60', '61-65', '66-70', '>=71 Years']
-cols = ['B', 'C', 'D', 'E']
+cols = ['B', 'C', 'D', 'E', 'F']
 col_names = band_name.columns[1:].tolist()
 
 #Excel Writer
@@ -126,10 +133,11 @@ worksheet.data_validation('B2', {'validate':'list',
 for i, age in enumerate(ages):
      worksheet.write(f'A{i+11}', age, bold_right)
 #Add band level vlookups
-worksheet.write_formula('B4', "=IFERROR(VLOOKUP(B2,'Band Data'!A:E,2,0), 0)", center_border)
-worksheet.write_formula('B5', "=IFERROR(VLOOKUP(B2,'Band Data'!A:E,3,0), 0)", center_border)
-worksheet.write_formula('B6', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:E,4,0), "-")''', center_border)
-worksheet.write_formula('B7', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:E,5,0), "-")''', center_border)
+worksheet.write_formula('B4', "=IFERROR(VLOOKUP(B2,'Band Data'!A:F,2,0), 0)", center_border)
+worksheet.write_formula('B5', "=IFERROR(VLOOKUP(B2,'Band Data'!A:F,3,0), 0)", center_border)
+worksheet.write_formula('B6', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:F,4,0), "-")''', center_border)
+worksheet.write_formula('B7', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:F,5,0), "-")''', center_border)
+worksheet.write_formula('B8', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:F,6,0), "-")''', center_border)
 
 #Add text for age band level lookups
 for i, col in enumerate(zip(cols, col_names)):
@@ -138,13 +146,15 @@ for i, col in enumerate(zip(cols, col_names)):
 #Add age band lookups
 for i in range(11,23):
      #Headcount
-     worksheet.write_formula(f'B{i}',f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:G,4,0), 0)''', center_border)
+     worksheet.write_formula(f'B{i}',f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:H,4,0), 0)''', center_border)
      #FTE
-     worksheet.write_formula(f'C{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:G,5,0), 0)''', center_border)
+     worksheet.write_formula(f'C{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:H,5,0), 0)''', center_border)
      #Average Time in Position
-     worksheet.write_formula(f'D{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:G,6,0), "-")''', center_border)
+     worksheet.write_formula(f'D{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:H,6,0), "-")''', center_border)
      #Longest Time in Position
-     worksheet.write_formula(f'E{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:G,7,0), "-")''', center_border)
+     worksheet.write_formula(f'E{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:H,7,0), "-")''', center_border)
+     #Pension Opt Out
+     worksheet.write_formula(f'F{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:H,8,0), "-")''', center_border)
 
 #Add in bar charts
 headcount_chart = workbook.add_chart({'type':'column'})
@@ -157,8 +167,17 @@ headcount_chart.add_series({'name':'Filter!$C$10',
 headcount_chart.set_title({'name':'Headcount and FTE'})
 headcount_chart.set_x_axis({'name':'Age Bands', 'num_font':{'rotation':45}})
 headcount_chart.set_style(2)
-worksheet.insert_chart('G2', headcount_chart, {'x_scale':1.04, 'y_scale':1.07})
+worksheet.insert_chart('H2', headcount_chart, {'x_scale':1.04, 'y_scale':1.07})
 
+pension_chart = workbook.add_chart({'type':'column'})
+pension_chart.add_series({'name':'Filter!$F$10',
+                            'categories': 'Filter!$A$11:$A$22',
+                            'values': 'Filter!$F$11:$F$22'})
+pension_chart.set_title({'name':'Pension Opt Out'})
+pension_chart.set_x_axis({'name':'Age Bands', 'num_font':{'rotation':45}})
+pension_chart.set_style(2)
+pension_chart.set_legend({'none':True})
+worksheet.insert_chart('H17', pension_chart, {'x_scale':1.04, 'y_scale':1.07})
 
 ####Add lookup sheets####
 band_name.to_excel(writer, sheet_name='Band Data', index=False, engine='xlsxwriter')
