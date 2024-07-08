e29bbddf57fc8f30e940ca5eb9f7e022cad9a9f6
Emily-OBrien-NHS <e.obrien6@nhs.net>
2024-07-08 15:17:26 +0100

    Add Plymouth only switch

--- employee_banding_postcodes.py
+++ employee_banding_postcodes.py
@@ -11,6 +11,9 @@ from datetime import datetime
 os.chdir('C:/Users/obriene/Projects/Mapping/Outputs')
 run_date = datetime.today().strftime('%Y-%m-%d')
 
+#Only map/export data for plymouth postcodes
+plymouth_only = False
+
 #readin postcode to latlong data
 pcode_LL = pd.read_csv("G:/PerfInfo/Performance Management/PIT Adhocs/2021-2022/Hannah/Maps/pcode_LSOA_latlong.csv",
                             usecols = ['pcds', 'lat', 'long'])
@@ -37,6 +40,11 @@ Band_pcds['Band Groups'] = np.select([((Band_pcds['Band No'] >= 1) & (Band_pcds[
 
 #Add in space and capitalise postcodes
 Band_pcds['pcds'] = [pcd.upper()  if ' ' in pcd else (pcd[:-3]+' '+pcd[-3:]).upper() for pcd in Band_pcds['pcds'].tolist()]
+Band_pcds['FTE'] = Band_pcds['FTE'].astype(float)
+total_fte, total_headcount = Band_pcds.agg({'FTE':'sum', 'Band Groups':'count'})
+#Filter to only plymouth postcodes if true
+if plymouth_only:
+     Band_pcds = Band_pcds.loc[Band_pcds['pcds'].str.contains(r'PL[0-9] ')].copy()
 
 #Merge data together, group up data to postcode up to last 2 charecters to keep annonymity
 LL_df = pcode_LL.merge(Band_pcds, on='pcds', how='inner')
@@ -81,13 +89,14 @@ def group_by_band(df, pen, cols):
      df['Average Time in Position'] = parse_date(df['Average Time in Position'])
      df['Max Time in Position'] = parse_date(df['Max Time in Position'])
      #Add pension data
+     df['Proportion of Total Headcount'] = (df['Headcount'] / total_headcount)
+     df['Proportion of Total FTE'] = (df['FTE'] / total_fte)
      df = df.merge(pen.groupby(cols, as_index=False)['Pension Opt Out'].count(), on=cols, how='left')
      return df
 
 #Select/add required columns
 df = Band_pcds[['Banding', 'Band No', 'Band Groups', 'FTE', 'AgeBand', 'StartDateInPosition']].copy()
 df['Days in Position'] = (pd.Timestamp.now() - pd.to_datetime(df['StartDateInPosition']))
-df['FTE'] = df['FTE'].astype(float)
 
 #Get the grouped tables required
 band_name = group_by_band(df, pension, ['Banding'])
@@ -99,11 +108,11 @@ age_band.insert(loc=0, column='lookup', value=lookup_col)
 ##############To excel ##################
 #Lists for formatting
 ages = ['<=20 Years', '21-25', '26-30', '31-35', '36-40', '41-45', '46-50', '51-55', '56-60', '61-65', '66-70', '>=71 Years']
-cols = ['B', 'C', 'D', 'E', 'F']
+cols = ['B', 'C', 'D', 'E', 'F', 'G', 'H']
 col_names = band_name.columns[1:].tolist()
 
 #Excel Writer
-writer = pd.ExcelWriter(f"C:/Users/obriene/Projects/Mapping/Outputs/{run_date}_aggregate_employee_band_data.xlsx", engine='xlsxwriter')
+writer = pd.ExcelWriter(f"C:/Users/obriene/Projects/Mapping/Outputs/{run_date}_aggregate_employee_band_data Plymouth Only.xlsx", engine='xlsxwriter')
 workbook = writer.book
 
 ####COVER SHEET####
@@ -114,6 +123,8 @@ center = workbook.add_format({'align':'center'})
 center_border = workbook.add_format({'align':'center', 'border':True})
 bold_right = workbook.add_format({'bold':True, 'align':'right', 'border':2})
 bold_wrap = workbook.add_format({'bold': True, 'align':'center', 'valign':'center', 'text_wrap':True, 'border':2})
+percent_format = workbook.add_format({"num_format": "0%", 'align':'center', 'border':True})
+percent_format2 = workbook.add_format({"num_format": "0%"})
 
 #Add filter worksheet
 worksheet = workbook.add_worksheet('Filter')
@@ -121,7 +132,7 @@ worksheet = workbook.add_worksheet('Filter')
 #Set general column formats
 worksheet.set_column(0, 27, 8, white)
 worksheet.set_column(0, 0, 21, white)
-worksheet.set_column(1, 4, 10, white)
+worksheet.set_column(1, 7, 10, white)
 
 #Add band drop down cell
 worksheet.write('A2', 'Band:', bold_right)
@@ -131,64 +142,72 @@ worksheet.data_validation('B2', {'validate':'list',
 
 #Add in text for band level lookups
 for i, age in enumerate(ages):
-     worksheet.write(f'A{i+11}', age, bold_right)
+     worksheet.write(f'A{i+13}', age, bold_right)
 #Add band level vlookups
-worksheet.write_formula('B4', "=IFERROR(VLOOKUP(B2,'Band Data'!A:F,2,0), 0)", center_border)
-worksheet.write_formula('B5', "=IFERROR(VLOOKUP(B2,'Band Data'!A:F,3,0), 0)", center_border)
-worksheet.write_formula('B6', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:F,4,0), "-")''', center_border)
-worksheet.write_formula('B7', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:F,5,0), "-")''', center_border)
-worksheet.write_formula('B8', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:F,6,0), "-")''', center_border)
+worksheet.write_formula('B4', "=IFERROR(VLOOKUP(B2,'Band Data'!A:H,2,0), 0)", center_border)
+worksheet.write_formula('B5', "=IFERROR(VLOOKUP(B2,'Band Data'!A:H,3,0), 0)", center_border)
+worksheet.write_formula('B6', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:H,4,0), "-")''', center_border)
+worksheet.write_formula('B7', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:H,5,0), "-")''', center_border)
+worksheet.write_formula('B8', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:H,6,0), "-")''', percent_format)
+worksheet.write_formula('B9', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:H,7,0), "-")''', percent_format)
+worksheet.write_formula('B10', '''=IFERROR(VLOOKUP(B2,'Band Data'!A:H,8,0), "-")''', center_border)
 
 #Add text for age band level lookups
 for i, col in enumerate(zip(cols, col_names)):
      worksheet.write(f'A{i+4}', col[1], bold_right)
-     worksheet.write(f'{col[0]}10', col[1], bold_wrap)
+     worksheet.write(f'{col[0]}12', col[1], bold_wrap)
 #Add age band lookups
-for i in range(11,23):
+for i in range(13,25):
      #Headcount
-     worksheet.write_formula(f'B{i}',f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:H,4,0), 0)''', center_border)
+     worksheet.write_formula(f'B{i}',f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:J,4,0), 0)''', center_border)
      #FTE
-     worksheet.write_formula(f'C{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:H,5,0), 0)''', center_border)
+     worksheet.write_formula(f'C{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:J,5,0), 0)''', center_border)
      #Average Time in Position
-     worksheet.write_formula(f'D{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:H,6,0), "-")''', center_border)
+     worksheet.write_formula(f'D{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:J,6,0), "-")''', center_border)
      #Longest Time in Position
-     worksheet.write_formula(f'E{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:H,7,0), "-")''', center_border)
+     worksheet.write_formula(f'E{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:J,7,0), "-")''', center_border)
+     #Proportion of Total Headcount
+     worksheet.write_formula(f'F{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:J,8,0), "-")''', percent_format)
+     #Proportion of FTE
+     worksheet.write_formula(f'G{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:J,9,0), "-")''', percent_format)
      #Pension Opt Out
-     worksheet.write_formula(f'F{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:H,8,0), "-")''', center_border)
+     worksheet.write_formula(f'H{i}', f'''=IFERROR(VLOOKUP((B2&" "&A{i}),'Age Band Data'!A:J,10,0), "-")''', center_border)
 
 #Add in bar charts
 headcount_chart = workbook.add_chart({'type':'column'})
-headcount_chart.add_series({'name':'Filter!$B$10',
-                            'categories': 'Filter!$A$11:$A$22',
-                            'values': 'Filter!$B$11:$B$22'})
+headcount_chart.add_series({'name':'Filter!$B$12',
+                            'categories': 'Filter!$A$13:$A$24',
+                            'values': 'Filter!$B$13:$B$24'})
 headcount_chart.add_series({'name':'Filter!$C$10',
-                            'categories': 'Filter!$A$11:$A$22',
-                            'values': 'Filter!$C$11:$C$22'})
+                            'categories': 'Filter!$A$13:$A$24',
+                            'values': 'Filter!$C$13:$C$24'})
 headcount_chart.set_title({'name':'Headcount and FTE'})
 headcount_chart.set_x_axis({'name':'Age Bands', 'num_font':{'rotation':45}})
 headcount_chart.set_style(2)
-worksheet.insert_chart('H2', headcount_chart, {'x_scale':1.04, 'y_scale':1.07})
+worksheet.insert_chart('J2', headcount_chart, {'x_scale':1.04, 'y_scale':1.07})
 
 pension_chart = workbook.add_chart({'type':'column'})
-pension_chart.add_series({'name':'Filter!$F$10',
-                            'categories': 'Filter!$A$11:$A$22',
-                            'values': 'Filter!$F$11:$F$22'})
+pension_chart.add_series({'name':'Filter!$H$12',
+                            'categories': 'Filter!$A$13:$A$24',
+                            'values': 'Filter!$H$13:$H$24'})
 pension_chart.set_title({'name':'Pension Opt Out'})
 pension_chart.set_x_axis({'name':'Age Bands', 'num_font':{'rotation':45}})
 pension_chart.set_style(2)
 pension_chart.set_legend({'none':True})
-worksheet.insert_chart('H17', pension_chart, {'x_scale':1.04, 'y_scale':1.07})
+worksheet.insert_chart('J17', pension_chart, {'x_scale':1.04, 'y_scale':1.07})
 
 ####Add lookup sheets####
 band_name.to_excel(writer, sheet_name='Band Data', index=False, engine='xlsxwriter')
 band_worksheet = writer.sheets['Band Data']
 band_worksheet.set_column(0, 0, 16)
-band_worksheet.set_column(1, 4, 11, center)
+band_worksheet.set_column(1, 7, 11, center)
+band_worksheet.set_column(5, 6, 16, percent_format2)
 
 age_band.to_excel(writer, sheet_name='Age Band Data', index=False, engine='xlsxwriter')
 age_worksheet = writer.sheets['Age Band Data']
 age_worksheet.set_column(0, 1, 26)
-age_worksheet.set_column(2, 6, 11, center)
+age_worksheet.set_column(2, 10, 11, center)
+age_worksheet.set_column(7, 8, 16, percent_format2)
 
 ####save and close the workbook####
 writer.close()
